8645fa39ec0007a061c16be723ab222ff2f98113
takuyataira0107 <146914986+takuyataira0107@users.noreply.github.com>
2024-06-10 12:58:29 +0900

    第08回完了

--- Assets/ItemScript.cs
+++ Assets/ItemScript.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ItemScript : MonoBehaviour
 {
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,7 +20,8 @@ public class ItemScript : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        DestroySelf();
+        animator.SetTrigger("Get");
+        //DestroySelf();
         //Debug.Log("Enter");
     }
     private void OnTriggerStay(Collider other)
